4a72bc3ec183ff235aa4d2158873a471beeaf949
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-08-25 17:33:21 -0400

    landmarkRow code

---
+++ LandmarksTutorial/Views/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  LandmarksTutorial
+//
+//  Created by Montserrat Gomez on 2023-08-25.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+	var landmark: Landmark
+	
+    var body: some View {
+		
+		HStack {
+			landmark.image
+				.resizable()
+				.frame(width: 50, height: 50)
+			Text(landmark.name)
+			Spacer()
+		}
+        
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+		LandmarkRow(landmark: landmarks[1])
+
+    }
+}
