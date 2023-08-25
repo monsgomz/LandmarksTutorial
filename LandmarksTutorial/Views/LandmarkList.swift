c3cf905794e10f831c05cf70509a67ecf66817ad
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-08-25 17:54:26 -0400

    New code for list view

---
+++ LandmarksTutorial/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  LandmarksTutorial
+//
+//  Created by Montserrat Gomez on 2023-08-25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+		
+		List {
+			LandmarkRow(landmark: landmarks[0])
+			LandmarkRow(landmark: landmarks[1])
+		}
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
